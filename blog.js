4806769f05859c2efc44482cd6a6f039b31ca5ad
Hansen09 <110557227+Hansen09@users.noreply.github.com>
2024-11-11 12:14:52 +0700

    edit

--- blog.js
+++ blog.js
@@ -11,3 +11,24 @@ function addComment() {
   localStorage.setItem("commentsList", JSON.stringify(commentsList));
   document.getElementById("commentInput").value = "";
 }
+
+function renderComments() {
+  const commentsListData = document.getElementById("commentsListData");
+  commentsListData.innerHTML = "";
+  let row = "";
+
+  commentsList.forEach((comment, index) => {
+    row += `
+        <tr>
+          <td>Name: ${comment.name}</td>
+          <td>Email: ${comment.email}</td>
+          <td>Comment: ${comment.comment}</td>
+        </tr>
+        <br>
+      `;
+  });
+
+  commentsListData.innerHTML = row;
+}
+
+renderComments();
